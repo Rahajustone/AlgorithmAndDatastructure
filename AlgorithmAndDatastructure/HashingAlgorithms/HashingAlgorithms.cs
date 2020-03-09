1499be18a669e8492b7bb77db59d4e04a03df272
rahajustone <kh.rahmatullo@gmail.com>
2020-03-09 09:59:56 -0500

    Hashing Algorithms

---
+++ AlgorithmAndDatastructure/HashingAlgorithms/HashingAlgorithms.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmAndDatastructure.HashingAlgorithms
+{
+    class HashingAlgorithms
+    {
+        // The simple hash methods
+        public int Hash(int x)
+        {
+            return x % 7;
+        }
+    }
+}
