c1f6a1c6fe17540dc4c500e47372587d2b22f2ba
rahajustone <kh.rahmatullo@gmail.com>
2024-02-08 22:37:17 -0500

    Add custom search

---
+++ AlgorithmAndDatastructure/SearchingAlgorithms/CustomSearching.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace AlgorithmAndDatastructure.SearchingAlgorithms
+{
+	public class CustomSearching
+	{
+		public CustomSearching()
+		{
+		}
+
+		public bool LinearSearch(int target, params int[] arr)
+		{
+			for (int i = 0; i < arr.Length; i++)
+			{
+				if (arr[i] == target) return true;
+			}
+
+			return false;
+		}
+
+		public bool BinarySearch(int target, params int[] arr)
+		{
+			int left = 0;
+			int right = arr.Length;
+
+			while (left<right)
+			{
+				int mid = (left + right) / 2;
+				if (arr[mid] == target) return true;
+				else if (arr[mid] > target) right = mid;
+				else left = mid + 1;
+			}
+
+			return false;
+		}
+	}
+}
+
