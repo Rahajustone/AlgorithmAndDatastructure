170fd80652c3ba772239efeb2f4824e44ad110f5
rahajustone <kh.rahmatullo@gmail.com>
2024-06-06 09:47:59 -0400

    Add a Queue implementation

---
+++ Algorithms.Princeton.Unuversity/QueueCustom/Queue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Princeton.Unuversity.QueueCustom
+{
+    public class Queue<T>
+    {
+        private Node<T> first, last;
+        private class Node<T>
+        {
+            public T item;
+            public Node<T> next;
+
+            public Node(T item)
+            {
+                this.item = item;
+            }
+        }
+
+        public void Enqueue(T item)
+        {
+            Node<T> Oldlast = last;
+            last = new Node<T>(item);
+            last.next = null;
+            if (IsEmpty())
+                first = last;
+            else
+                Oldlast.next = last;
+
+        }
+        public T Dequeue()
+        {
+            T item = first.item;
+            first = first.next;
+
+            if (IsEmpty()) last = null;
+
+            return item;
+        }
+
+        bool IsEmpty()
+        {
+            return first == null;
+        }
+
+    }
+}
